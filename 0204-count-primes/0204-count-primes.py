59a54ac8d968af6c7eb68f7c274264751aa8ba23
Nayanjit <75443741+Nayanjit-Sarkar@users.noreply.github.com>
2023-09-15 17:03:11 +0530

    Time: 2717 ms (47.99%), Space: 70 MB (42.78%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countPrimes(self, n: int) -> int:
+        c = 0
+        p = 2
+        arr = [True for i in range(n+1)]
+        while (p*p< n):
+            if arr[p]:
+                for i in range(p*p, n, p):
+                    arr[i] = False
+            p+=1
+        
+        for i in range(2, n):
+            if arr[i]:
+                c+=1
+        return c
